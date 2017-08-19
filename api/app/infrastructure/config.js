34c2ce997f8e20919e2e1dc706f5ead84fd2ae2b
Hypernikao <fce+git@octo.com>
2017-08-19 09:32:11 +0200

    Add config.js

---
+++ api/app/infrastructure/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    server: {
+        host: '127.0.0.1',
+        port: 8080
+    },
+    database: {
+        host: '127.0.0.1',
+        port: 27017,
+        db: 'potluck',
+        username: '',
+        password: ''
+    }
+};
