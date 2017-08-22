412ab50d464736516a1d3037c95448130b9afff9
Hypernikao <fce+git@octo.com>
2017-08-22 14:14:11 +0200

    Add host-service creation

--- api/app/domain/services/host-service.js
+++ api/app/domain/services/host-service.js
@@ -1,5 +1,13 @@
+require('rootpath')();
+const UserRepository = require('app/infrastructure/repositories/user-repository');
+
 module.exports = {
-    createHost() {
-        return Promise.resolve();
+    createHost(user) {
+
+        return UserRepository
+            .save(user)
+            .catch((err) => {
+                return err;
+            });
     }
 }
