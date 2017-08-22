6f59f0b2708f60d607c6b97dd03007f82f9e04df
Hypernikao <fce+git@octo.com>
2017-08-22 14:15:10 +0200

    Chai-as-promised

--- api/tests/helper.js
+++ api/tests/helper.js
@@ -1,5 +1,8 @@
 const sinon = require('sinon');
-const { expect } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-as-promised'));
+
 const { describe, it, beforeEach, afterEach, before, after } = require('mocha');
 
 module.exports = {
