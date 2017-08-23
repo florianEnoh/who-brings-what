d95510285a0cc29b77cbaf7a4e75867c914d6271
Hypernikao <fce+git@octo.com>
2017-08-23 18:35:50 +0200

    Refacto user repository

--- api/app/infrastructure/repositories/user-repository.js
+++ api/app/infrastructure/repositories/user-repository.js
@@ -1,6 +1,16 @@
+const User = require('../../domain/models/user');
+
 module.exports = {
 
-    save() {
+    save(user) {
+        return new Promise((resolve, reject) => {
+            return new User(user).save((err, user) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(user._id);
+            });
+        });
 
     }
 };
