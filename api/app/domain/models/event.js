b4d73864349f4f9648fd69bd424babee37ad63cd
Hypernikao <fce+git@octo.com>
2017-08-25 19:15:50 +0200

    Add custom message to event model

--- api/app/domain/models/event.js
+++ api/app/domain/models/event.js
@@ -2,14 +2,15 @@ const Mongoose = require('mongoose'),
     Schema = Mongoose.Schema;
 
 const eventSchema = new Schema({
-    title: { type: String, required: true },
+    title: { type: String, required: [true, 'A title is required'] },
     location: String,
     date: String,
     url: String,
-    hostId: { type: Schema.Types.ObjectId, required: true },
+    hostId: { type: Schema.Types.ObjectId, required: [true, 'A host is required'] },
     guestsIds: [{ type: Schema.Types.ObjectId }],
     needs: [{ name: String, quantity: Number }],
-    comments: String
+    comments: String,
+    createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = Mongoose.models.events || Mongoose.model('events', eventSchema);
