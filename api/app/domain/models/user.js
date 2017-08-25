989bd2bd62e1cfb3a15135c12b928843fa29bfc0
Hypernikao <fce+git@octo.com>
2017-08-25 19:20:18 +0200

    Add custom validation error message to user model

--- api/app/domain/models/user.js
+++ api/app/domain/models/user.js
@@ -2,9 +2,10 @@ const Mongoose = require('mongoose'),
     Schema = Mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: String, required: true },
-    email: { type: String },
-    password: { type: String }
+    username: { type: String, required: [true, 'Username is not valid'] },
+    email: { type: String, validate: [/\S+@\S+\.\S/, 'Email is not valid'] },
+    password: { type: String },
+    createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = Mongoose.models.users || Mongoose.model('users', UserSchema);
