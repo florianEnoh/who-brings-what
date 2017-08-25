004794a02aa1fb11c383e854671ab6d052f3a005
Hypernikao <fce+git@octo.com>
2017-08-25 22:48:53 +0200

    Simplify user model

--- api/app/domain/models/user.js
+++ api/app/domain/models/user.js
@@ -2,10 +2,13 @@ const Mongoose = require('mongoose'),
     Schema = Mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: String, required: [true, 'Username is not valid'] },
-    email: { type: String, validate: [/\S+@\S+\.\S/, 'Email is not valid'] },
+    username: {
+        type: String,
+        required: true
+    },
+    email: { type: String, validate: [/\S+@\S+\.\S/] },
     password: { type: String },
     createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = Mongoose.models.users || Mongoose.model('users', UserSchema);
+module.exports = Mongoose.models.User || Mongoose.model('User', UserSchema);
