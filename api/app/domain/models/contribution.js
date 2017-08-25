99c8be093fb3ccf3e18a7efcb9d13fab1a01c4af
Hypernikao <fce+git@octo.com>
2017-08-25 22:43:10 +0200

    Simplify contribution model

--- api/app/domain/models/contribution.js
+++ api/app/domain/models/contribution.js
@@ -2,9 +2,10 @@ const Mongoose = require('mongoose'),
     Schema = Mongoose.Schema;
 
 const contributionSchema = new Schema({
-    eventId: { type: Schema.Types.ObjectId, required: true },
-    userId: { type: Schema.Types.ObjectId, required: true },
-    involvements: [{ name: String, quantity: Number }]
+    eventId: { type: Schema.Types.ObjectId, ref: 'Event', require: true },
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    involvements: [{ name: String, quantity: Number }],
+    createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = Mongoose.models.contributions || Mongoose.model('contributions', contributionSchema);
+module.exports = Mongoose.models.Contribution || Mongoose.model('Contribution', contributionSchema);
