4394772d90e017a0fad11f5292f819cd2790ae71
Hypernikao <fce+git@octo.com>
2017-08-26 17:43:08 +0200

    Add little error management in event route

--- api/app/features/events/eventHandler.js
+++ api/app/features/events/eventHandler.js
@@ -4,14 +4,19 @@ const eventService = require('../../domain/services/event-service');
 
 module.exports = {
     create(request, reply) {
-        const { user } = request.payload;
-        const { event } = request.payload;
-
         return hostService
-            .createHost(user)
-            .then((userId) => {
-                const eventDetails = Object.assign({}, { userId }, event);
-                return eventService.createEvent(eventDetails);
+            .createHost(request.payload.user)
+            .then(({ _id: userId }) => {
+                return eventService.createEvent(userId, request.payload.event);
+            })
+            .then(url => reply(url).code(201))
+            .catch((err) => {
+                const statusCode = _getErrorStatusCode(err);
+                reply(err).code(statusCode);
             });
     }
 };
+
+function _getErrorStatusCode(err) {
+    return ('name' in err) ? 500 : 422;
+}
