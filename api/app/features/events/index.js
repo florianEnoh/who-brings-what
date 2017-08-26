6df9d404152aa8848ceb522023451527ff415504
Hypernikao <fce+git@octo.com>
2017-08-26 17:41:55 +0200

    add joi config to event route

--- api/app/features/events/index.js
+++ api/app/features/events/index.js
@@ -1,4 +1,5 @@
 const eventHandler = require('./eventHandler');
+const Joi = require('joi');
 
 exports.register = function(server, options, next) {
     server.route({
@@ -6,6 +7,12 @@ exports.register = function(server, options, next) {
         path: '/events',
         config: {
             handler: eventHandler.create,
+            validate: {
+                payload: {
+                    host: Joi.object().required().label('A valid host is required'),
+                    event: Joi.object().required().label('A valid event details are required')
+                }
+            },
             tags: ['api']
         }
 
