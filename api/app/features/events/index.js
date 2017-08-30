c0a72686e3123df6b780146a18d2cf4e81819d8c
Hypernikao <fce+git@octo.com>
2017-08-30 09:51:15 +0200

    fix new endpoint

--- api/app/features/events/index.js
+++ api/app/features/events/index.js
@@ -2,21 +2,37 @@ const eventHandler = require('./eventHandler');
 const Joi = require('joi');
 
 exports.register = function(server, options, next) {
-    server.route({
-        method: 'POST',
-        path: '/events',
-        config: {
-            handler: eventHandler.create,
-            validate: {
-                payload: {
-                    host: Joi.object().required(),
-                    event: Joi.object().required()
-                }
-            },
-            tags: ['api']
+    server.route([{
+            method: 'POST',
+            path: '/events',
+            config: {
+                handler: eventHandler.create,
+                validate: {
+                    payload: {
+                        host: Joi.object().required(),
+                        event: Joi.object().required()
+                    }
+                },
+                tags: ['api']
+            }
+        },
+        {
+            method: 'POST',
+            path: '/events/{id}/guests',
+            config: {
+                handler: eventHandler.addGuest,
+                validate: {
+                    params: {
+                        id: Joi.number()
+                    },
+                    payload: {
+                        guest: Joi.object().required()
+                    }
+                },
+                tags: ['api']
+            }
         }
-
-    });
+    ]);
 
     return next();
 };
